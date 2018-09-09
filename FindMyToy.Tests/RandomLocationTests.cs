af96bfa3a6d8dd7881f2c018878fba4d09394a4e
Pietn <pietn@msn.com>
2018-09-09 21:26:33 +0200

    make theory for number of locations in range

--- FindMyToy.Tests/RandomLocationTests.cs
+++ FindMyToy.Tests/RandomLocationTests.cs
@@ -34,6 +34,18 @@ public class RandomLocationTests
   }
 
   [Theory]
+  [InlineData(1, 1)]
+  [InlineData(2, 3)]
+  public void WhenRangeIsRange_ThenRandomLocationsIsNumberOfLocations(int range, int numberOfLocations)
+  {
+    var random = new Mock<IRandom>();
+    var randomLocation = new RandomLocation(random.Object);
+    randomLocation.GetInRange(range);
+
+    random.Verify(t => t.GetNext(0, numberOfLocations), Times.AtLeastOnce);
+  }
+
+  [Theory]
   [InlineData(0, 0, 0)]
   [InlineData(1, 0, 1)]
   [InlineData(2, 1, 0)]
