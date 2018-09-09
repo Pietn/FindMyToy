e6ef81152dd649e9c50cfe03e094ccc7d64f6706
Pietn <pietn@msn.com>
2018-09-09 21:28:33 +0200

    remove separated number of locations in range tests

--- FindMyToy.Tests/RandomLocationTests.cs
+++ FindMyToy.Tests/RandomLocationTests.cs
@@ -3,36 +3,6 @@ using Xunit;
 
 public class RandomLocationTests
 {
-  [Fact]
-  public void WhenRangeIs1_ThenRandomLocationsIs1()
-  {
-    var random = new Mock<IRandom>();
-    var randomLocation = new RandomLocation(random.Object);
-    randomLocation.GetInRange(1);
-
-    random.Verify(t => t.GetNext(0, 1), Times.AtLeastOnce);
-  }
-
-  [Fact]
-  public void WhenRangeIs2_ThenRandomLocationsIs3()
-  {
-    var random = new Mock<IRandom>();
-    var randomLocation = new RandomLocation(random.Object);
-    randomLocation.GetInRange(2);
-
-    random.Verify(t => t.GetNext(0, 3), Times.AtLeastOnce);
-  }
-
-  [Fact]
-  public void WhenRangeIs3_ThenRandomLocationsIs11()
-  {
-    var random = new Mock<IRandom>();
-    var randomLocation = new RandomLocation(random.Object);
-    randomLocation.GetInRange(2);
-
-    random.Verify(t => t.GetNext(0, 3), Times.AtLeastOnce);
-  }
-
   [Theory]
   [InlineData(1, 1)]
   [InlineData(2, 3)]
